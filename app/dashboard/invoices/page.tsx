6c0a995ceee4f27e1c01e377fb49933e223e4880
Amere <ahmedamerxx@gmail.com>
2024-01-24 00:39:08 +0200

    Initial commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+const InvoicesPage: FC = () => {
+    return (
+        <div>
+            <h1>Invoices Page</h1>
+            {/* Add your invoice components and logic here */}
+        </div>
+    );
+};
+
+export default InvoicesPage;
